2fc3d84edff75c4f7aac9d889a2f45e2b9373d7a
thangstad <75758239+thangstad@users.noreply.github.com>
2023-04-24 15:08:49 +0200

    Update terminalPrompt.py

--- terminalPrompt.py
+++ terminalPrompt.py
@@ -69,7 +69,7 @@ def main():
             print("Exiting...")
             break
         else:
-            print("Invalid choice. Please enter 1, 2, 3, 4, or 5.")
+            print("Invalid choice. Please enter 1, 2, 3, or 4")
 
 
 if __name__ == '__main__':
